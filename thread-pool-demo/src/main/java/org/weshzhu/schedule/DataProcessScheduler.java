c84164c9f243b7b75fe8e4495848c81a34f3038e
DemonZSD <498537806@qq.com>
2022-05-22 21:13:39 +0800

    喝多了

--- thread-pool-demo/src/main/java/org/weshzhu/schedule/DataProcessScheduler.java
+++ thread-pool-demo/src/main/java/org/weshzhu/schedule/DataProcessScheduler.java
@@ -12,6 +12,9 @@ import org.weshzhu.task.DataProcessTask;
 
 import java.lang.reflect.Proxy;
 
+/**
+ * 数据处理任务，任务调度配置类
+ */
 @Component
 @EnableScheduling
 public class DataProcessScheduler implements SchedulingConfigurer {
@@ -23,11 +26,14 @@ public class DataProcessScheduler implements SchedulingConfigurer {
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         Trigger trigger = triggerContext -> {
             CronTrigger cronTrigger = new CronTrigger("0 0/1 * * * ?");
+            System.out.println("------------------------" + cronTrigger.getExpression());
             return cronTrigger.nextExecutionTime(triggerContext);
         };
         ClassLoader classLoader = task.getClass().getClassLoader();
         Class[] interfaces = task.getClass().getInterfaces();
+        // 通过反射进行获取 runnable 实现类对象
         Runnable proxy = (Runnable) Proxy.newProxyInstance(classLoader, interfaces, new SchedulerRunnableProxy(task, "data-process"));
         taskRegistrar.addTriggerTask(proxy, trigger);
+
     }
 }
