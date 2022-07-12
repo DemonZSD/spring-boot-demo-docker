94ac5020a5acfb9a8f5dfc43a25c3373f2e683f1
DemonZSD <498537806@qq.com>
2022-07-12 21:34:34 +0800

    Add mysql-util-doc

---
+++ mysql-utils/mysql-util-doc/src/main/java/org/weshzhu/dbtool/doc/mysqldbdoc/MysqlDbDocApplication.java
@@ -0,0 +1,13 @@
+package org.weshzhu.dbtool.doc.mysqldbdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlDbDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlDbDocApplication.class, args);
+	}
+
+}
---
+++ mysql-utils/mysql-util-doc/src/test/java/org/weshzhu/dbtool/doc/mysqldbdoc/MysqlDbDocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.weshzhu.dbtool.doc.mysqldbdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlDbDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
